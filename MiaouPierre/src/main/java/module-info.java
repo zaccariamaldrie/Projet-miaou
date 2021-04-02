b462dbea545328edaa714e13eb0dfa3791c3156b
Sesax <31807206+Sesax@users.noreply.github.com>
2021-04-02 08:57:50 +0200

    Delete MiaouPierre directory

--- MiaouPierre/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module fr.miaou {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires mysql.connector.java;
-
-    opens fr.miaou to javafx.fxml;
-    exports fr.miaou;
-}
